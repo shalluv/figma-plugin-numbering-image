be186caa3a27914957503803f80ff6e12819b057
shalluv <leon555thai@gmail.com>
2023-11-17 12:55:21 +0700

    fix: generated number is out of frame

--- code.ts
+++ code.ts
@@ -7,14 +7,15 @@
   for (let i = 0; i < nodes.length; ++i) {
     const text = figma.createText();
     text.name = nodes[i].name.split(" ")[1];
-    text.x = nodes[i].absoluteTransform[0][2];
-    text.y = nodes[i].absoluteTransform[1][2];
+    text.x = nodes[i].x;
+    text.y = nodes[i].y;
     text.characters = nodes[i].name.split(" ")[1];
 
     text.fontSize = 24;
     text.fills = [{ type: "SOLID", color: { r: 1, g: 1, b: 1 } }];
     text.strokes = [{ type: "SOLID", color: { r: 0, g: 0, b: 0 } }];
     text.strokeWeight = 2;
+    nodes[i].parent?.appendChild(text);
   }
 })().then(() => {
   figma.closePlugin();
